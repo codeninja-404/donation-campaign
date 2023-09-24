f7fe4f523286458b392d4dcafefdd21b1d287bd8
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-09-24 23:10:17 +0600

    added json data to the project, created route

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/Header/NavBar";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="bg-green-500 container mx-auto px-2 lg:max-w-[1280px]">
             this is main layout
+            <NavBar></NavBar>
+            <div>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
+import Home from "../Page/Home/Home";
+import Donation from "../Page/Donation/Donation";
+import Statistics from "../Page/Statistics/Statistics";
 
 const appRoute = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children:[
+        {
+           path:'/' ,
+           element:<Home></Home>
+        },
+        {
+           path:'/donation' ,
+           element:<Donation></Donation>
+        },
+        {
+           path:'/statistics' ,
+           element:<Statistics></Statistics>
+        }
+    ]
   },
 ]);
 
